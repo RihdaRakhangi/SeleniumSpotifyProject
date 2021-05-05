bf3c54435579cd9f61e134cb6a12e730d9f96401
RihdaRakhangi <83604327+RihdaRakhangi@users.noreply.github.com>
2021-05-05 21:24:18 +0530

    Add files via upload

---
+++ TC_SpotifyPage_01.java
@@ -0,0 +1,66 @@
+package com.qa.scripts;
+
+import java.io.IOException;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TC_SpotifyPage_01 extends TestBase {
+
+	@Test(dataProvider = "getData2")
+	public void CreateAccount(String email, String conEmail , String password, String displayName  , String year, String month , String day) throws InterruptedException {
+		
+		driver.get("https://www.spotify.com/in-en/free/");
+		
+		spotifyPage.getSignUpLink().click();
+		
+		
+		spotifyPage.getemailField().sendKeys(email);
+		Thread.sleep(1000);
+		
+		
+		spotifyPage.getConfirmEmail().sendKeys(conEmail);
+		Thread.sleep(1000);
+		
+		
+		spotifyPage.getPass().sendKeys(password);
+		Thread.sleep(1000);
+		
+		spotifyPage.getDisName().sendKeys(displayName);
+		
+		spotifyPage.getYear().sendKeys(year);
+		
+		spotifyPage.selectMonth(month);
+		
+		spotifyPage.getDay().sendKeys(day);
+		
+		
+		spotifyPage.getGender().click();
+		Thread.sleep(1000);
+		
+		spotifyPage.getChkbox().click();
+		Thread.sleep(1000);
+		
+		//driver.findElement(By.xpath("//*[@id=\"recaptcha-anchor\"]/div[1]")).click();
+		Thread.sleep(4000);
+	}
+	
+	/*@DataProvider
+	public String[][] getData2() throws IOException{
+		
+		String xlPath="Desktop";
+		String xlSheet = "Sheet1";
+		
+		int rowCount = ExcelUtility.getRowCount(xlPath, xlSheet);
+		int cellCount = ExcelUtility.getCellCount(xlPath, xlSheet, rowCount);
+		 
+		String [][] data = new String[rowCount][cellCount];
+		
+		for(int i=1;i<=rowCount;i++) {
+			for(int j=0;j<cellCount;j++) {
+				data[i-1][j] = ExcelUtility.getCellData(xlPath, xlSheet, i, j);
+			}
+		}
+		return data;*/
+		
+	}
