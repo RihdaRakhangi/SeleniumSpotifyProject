fa419f16161f10474ad723dabcc0cebc0a62e4cd
RihdaRakhangi <83604327+RihdaRakhangi@users.noreply.github.com>
2021-05-05 21:32:29 +0530

    Add files via upload

---
+++ TestBase.java
@@ -0,0 +1,45 @@
+package com.qa.scripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+//import com.beust.jcommander.Parameters;
+
+public class TestBase {
+	WebDriver driver;
+
+	SpotifyPage spotifyPage;
+	
+	//@Parameters({"Browser","Url"})
+	@BeforeClass
+	public void setup(String Browser, String url)  {
+		
+		if(Browser.equalsIgnoreCase("chrome")) {
+		System.setProperty("webdriver.chrome.driver","C:\\webdriver90\\chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		}
+		
+		else if(Browser.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.chrome.driver","C:\\webdriver90\\firefoxdriver.exe");
+			
+			driver = new ChromeDriver();
+			}
+		
+		driver.get(url);
+		
+		driver.manage().window().maximize();
+	
+		
+		spotifyPage = new SpotifyPage(driver);
+	}
+	
+	
+	
+	@AfterClass
+	public void tearDown() {
+		driver.close();
+	}
+}
